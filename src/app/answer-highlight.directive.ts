a9bdb0fdc688e34bd91c611cac930e5bf0b2d7a9
Dmitry <dmitrypolyakov1985@gmail.com>
2021-12-12 12:43:13 -0700

    app complete

---
+++ src/app/answer-highlight.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { map, filter } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appAnswerHighlight]',
+})
+export class AnswerHighlightDirective implements OnInit {
+  constructor(private el: ElementRef, private controlName: NgControl) {}
+
+  ngOnInit() {
+    this.controlName.control?.parent?.valueChanges
+      .pipe(
+        map(({ a, b, answer }) => {
+          return Math.abs((a + b - answer) / (a + b));
+        })
+      )
+      .subscribe((val) => {
+        if (val < 0.2) {
+          this.el.nativeElement.classList.add('close');
+        } else {
+          this.el.nativeElement.classList.remove('close');
+        }
+      });
+  }
+}
